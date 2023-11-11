3e54fdd39a8227e491986526c0db0bac1b634976
AmightyHal <46722329+AmightyHal@users.noreply.github.com>
2023-11-10 21:48:22 -0500

    Update # Chapter 6.py

--- # Chapter 6.py
+++ # Chapter 6.py
@@ -19,3 +19,53 @@ output_filename = 'myname.txt'
 my_name = 'Ethan Meeuwenberg'
 # Call the function to write the name to the file
 write_name_to_file(output_filename, my_name)
+
+# problem 2
+# Function to read the file
+def read_name_from_file(filename):
+    try:
+        # Open the file in read mode ('r')
+        with open(filename, 'r') as file:
+            # Read the content (name) from the file
+            name = file.read()
+            # Display the name on the screen
+            print(f"Read the name from {filename}: {name}")
+    except Exception as e:
+        # If an error occurs, print an error message
+        print(f"Error: {e}")
+
+# Specify the filename to read from
+input_filename = 'myname.txt'
+
+# Call the function to read the name from the file and display it
+read_name_from_file(input_filename)
+
+# programming excercises
+# ex 1
+# Function to display numbers from the file
+def display_numbers_from_file(filename):
+    try:
+        # Open the file in read mode ('r')
+        with open(filename, 'r') as file:
+            # Read all lines from the file
+            lines = file.readlines()
+
+            # Iterate through each line and display the numbers
+            for line in lines:
+                # Split the line into individual numbers
+                numbers = line.split()
+
+                # Display each number on the screen
+                for number in numbers:
+                    print(number)
+
+    except Exception as e:
+        # If an error occurs, print an error message
+        print(f"Error: {e}")
+
+# Define the filename to read from
+input_filename = 'numbers.txt'  # Change this to your actual filename
+
+# Call the function to display all numbers from the file
+display_numbers_from_file(input_filename)
+
