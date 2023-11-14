9177f1ae34f5f3f1c08e1dd6442cd58cc76136a2
AmightyHal <46722329+AmightyHal@users.noreply.github.com>
2023-11-13 22:49:10 -0500

    Update Chapter 7.py

--- Chapter 7.py
+++ Chapter 7.py
@@ -147,3 +147,16 @@ def get_user_numbers():
                 print("Invalid input. Please enter a valid number.")
 
     return numbers
+def analyze_numbers(numbers):
+    # Calculate the required statistics
+    lowest_number = min(numbers)
+    highest_number = max(numbers)
+    total = sum(numbers)
+    average = total / len(numbers)
+
+    # Display the results
+    print(f"\nLowest number: {lowest_number}")
+    print(f"Highest number: {highest_number}")
+    print(f"Total of the numbers: {total}")
+    print(f"Average of the numbers: {average:.2f}")
+
