58509d3cec30a935548501b83b5d7a64fbcfb63d
AmightyHal <46722329+AmightyHal@users.noreply.github.com>
2023-11-13 21:43:38 -0500

    Create Chapter 7.py

---
+++ Chapter 7.py
@@ -0,0 +1,26 @@
+# chapter 7 hw
+# algo workbench 8
+
+# first we will create the variable list1
+list1 = [1, 2, 3, 4, 5]
+# next we will use list comprehension to create a new list containing squares of numbers in list1
+# Using x**2 will calculate squares of each element x in list1
+squared_list = [x**2 for x in list1]
+# print the squared list for our results
+print(squared_list)
+
+#algo workbench 9
+# first we will create the variable list1
+list1 = [90, 110, 200, 150, 60, 84]
+# next use list comprehension to create a new list containing elements greater than 100
+greater_than_100 = [x for x in list1 if x > 100]
+# print our list resulting in numbers greater than 100
+print(greater_than_100)
+
+# programing exercise 1
+# first we will define a function to ask the user for sales per day to calculate weekly sales
+def calc_weekly_sales():
+    # we will need an empty list to store our daily sales in
+    daily_sales = []
+    # now we will prompt the user to enter sales for every day of the week
+    for day in ['Mon', 'Tues']
