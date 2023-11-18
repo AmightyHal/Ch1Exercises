38c06fb0c2045b9b7f94bd0b383a9a7adb7db351
AmightyHal <46722329+AmightyHal@users.noreply.github.com>
2023-11-17 19:03:13 -0500

    Update # Chapter 8.py

--- # Chapter 8.py
+++ # Chapter 8.py
@@ -1,2 +1,7 @@
 # Chapter 8
 # Algo Workbench 1
+if choice == 'Y' or choice == 'y':
+# To rewrite this statement we will use the choice.lower() function so we only have to check for the lowercase value
+if choice.lower() == 'y':
+
+# Algo Workbench 2
