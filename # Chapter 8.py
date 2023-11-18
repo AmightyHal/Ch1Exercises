465672ec31e3d3b62e7b2208e912f222a098e0d2
AmightyHal <46722329+AmightyHal@users.noreply.github.com>
2023-11-17 19:17:53 -0500

    Update # Chapter 8.py

--- # Chapter 8.py
+++ # Chapter 8.py
@@ -5,3 +5,18 @@ if choice == 'Y' or choice == 'y':
 if choice.lower() == 'y':
 
 # Algo Workbench 2
+# Define my_string with a sample statement that includes spaces to count
+my_string = "Hello there are spaces here to count :D."
+
+# Define a variable to count the spaces
+space_count = 0
+
+# Now we will go through each character in the string
+for char in my_string:
+    # Check current character to see if it's a space
+    if char == ' ':
+        # If a space is found add 1 to the space_count variable
+        space_count += 1
+
+# Print final count of space characters
+print("Number of spaces:", space_count)
